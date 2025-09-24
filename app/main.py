eb1aaecbf2f305b7f50591727f10b948e90e81b0
Sally Musemeche <sally@NEW-MAC-MINI.local>
2025-09-24 06:28:40 -0700

    Add /auth/echo to hash/mask envs for oauth debug

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from app.routers.bulk_photos_endpoint import router as bulk_router
 from app.routers.auth_debug_endpoint import router as auth_debug_router
 from app.routers.ub_monitor_endpoint import router as ub_monitor_router
+from app.routers.auth_echo_endpoint import router as auth_echo_router
 
 app = FastAPI()
 app.include_router(bulk_router)
 app.include_router(auth_debug_router)
 app.include_router(ub_monitor_router)
+app.include_router(auth_echo_router)
 
 @app.get("/")
 def root():
